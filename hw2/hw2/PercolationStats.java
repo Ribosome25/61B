ef09f65a9f251b145ef3183b15b6cd217faa7726
Ribosome25 <44216320+Ribosome25@users.noreply.github.com>
2020-10-18 09:10:10 -0500

    HW2 3rd try

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,45 @@
 package hw2;
 
+import  edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
 
+    private double[] results;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        results = new double[T];
+        for (int i=0; i<T; i++) {
+            Percolation p = pf.make(N);
+            while (true) {
+                int x = StdRandom.uniform(N);
+                int y = StdRandom.uniform(N);
+                p.open(x, y);
+                if (p.percolates()) {
+                    double percentage = p.numberOfOpenSites() / (N*N);
+                    results[i] = percentage;
+                    break;
+                }
+            }
+        }
+    }  // perform T independent experiments on an N-by-N grid
+    public double mean() {
+        return StdStats.mean(results);
+    }                                           // sample mean of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(results);
+    }                                         // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        double mean = mean();
+        double std = stddev();
+        return mean - 1.96*std;
+    }                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double mean = mean();
+        double std = stddev();
+        return mean + 1.96*std;
+    }                                 // high endpoint of 95% confidence interval
 }
